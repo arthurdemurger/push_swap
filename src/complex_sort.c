b88dd1eb053e6b7cdd490b5758c0b8dbe985c116
lirockk <98538729+lirockk@users.noreply.github.com>
2022-06-12 14:58:01 +0200

    Opti with rr & rrr

--- src/complex_sort.c
+++ src/complex_sort.c
@@ -6,16 +6,16 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 01:00:08 by ademurge          #+#    #+#             */
-/*   Updated: 2022/06/11 16:03:01 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/06/12 14:43:34 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
 	/*
-	**** 1. The stack A must be divided in two by the median number of the stack
-	**** 2. Send all the numbers <= the median number to the stack B,
-			but efficiently, until there are <= 3 numbers in the stack.
+	**** 1. The stack A must be divided in chunks
+	**** 2. Send all the numbers <= the max number of the chunk to the stack B,
+			but efficiently, until there are <= 5 numbers in the stack.
 			It is necessary to optimize the moves.
 	**** 3. Here, we will start to have fun.
 			You have to push to A but in the most optimized way possible,
@@ -53,15 +53,27 @@ void	push_split(t_stack *stacks)
 {
 	while (ft_lstsize(stacks->b) > 1)
 	{
-		if (stacks->b->data < (stacks->b->next)->data
+		while (stacks->b->data < (stacks->b->next)->data
 			&& (stacks->b->next)->data > ft_lstlast(stacks->b)->data)
-			ft_rotate(&stacks->b, B, SIMPLE);
-		else if (stacks->b->data < (stacks->b->next)->data
+		{
+			if (stacks->a->data < (stacks->b->next)->data)
+				ft_double(stacks, ROTATE);
+			else
+				ft_rotate(&stacks->b, B, SIMPLE);
+		}
+		if (stacks->b->data < (stacks->b->next)->data
 			&& (stacks->b->next)->data < ft_lstlast(stacks->b)->data)
 			ft_reverse_rot(&stacks->b, B, SIMPLE);
 		while (stacks->b->data > (stacks->b->next)->data
 			&& stacks->b->data < ft_lstlast(stacks->b)->data)
-			ft_reverse_rot(&stacks->b, B, SIMPLE);
+		{
+			if (ft_lstlast(stacks->a)->data < stacks->a->data
+				&& ft_lstlast(stacks->a) > ft_lstlast(stacks->b)
+				&& stacks->b->data < ft_lstlast(stacks->b)->data)
+				ft_double(stacks, REVERSE);
+			else
+				ft_reverse_rot(&stacks->b, B, SIMPLE);
+		}
 		smart_push(stacks);
 	}
 	smart_push(stacks);
